c383e1a3976fb49ee8baa2329a3cea64d046b09b
Henrique Baltar <82537763+HenriqueBaltar@users.noreply.github.com>
2021-09-13 22:58:25 -0300

    Update main.c
    Adjusting comments.

--- blinkLED_PWM/src/main.c
+++ blinkLED_PWM/src/main.c
@@ -8,7 +8,7 @@
 
 void app_main() {
 
-    /*Prepare and set configuration of timers that will be used by LED Controller*/
+    // Prepare and set configuration of timers that will be used by LED Controller
     ledc_timer_config_t ledc_timer = {
         .duty_resolution = LEDC_TIMER_8_BIT, // resolution of PWM duty
         .freq_hz = 500,                      // frequency of PWM signal
@@ -37,17 +37,17 @@ void app_main() {
         ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0);
         vTaskDelay(500 / portTICK_PERIOD_MS); // 0.5s at 25%
 
-        ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, 128); // 0-15
+        ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, 128);
         ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0);
         vTaskDelay(500 / portTICK_PERIOD_MS); // 0.5s at 50%
 
-        ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, 191); // 0-15
+        ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, 191);
         ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0);
-        vTaskDelay(500 / portTICK_PERIOD_MS); // 0.5s at 50%
+        vTaskDelay(500 / portTICK_PERIOD_MS); // 0.5s at 75%
 
-        ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, 255); // 0-15
+        ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, 255);
         ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0);
-        vTaskDelay(500 / portTICK_PERIOD_MS); // 0.5s at 50%
+        vTaskDelay(500 / portTICK_PERIOD_MS); // 0.5s at 100%
 
     }
 }
